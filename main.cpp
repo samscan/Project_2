85f70e41a0b791901f270078dd2b095f37b22a73
Onigbogj <onigbogj@union.edu>
2024-01-25 23:27:23 +0000

    changes

--- main.cpp
+++ main.cpp
@@ -2,6 +2,20 @@
 #include "mbed.h"
 #include "arm_book_lib.h"
 
+#define TIME_INCREMENT_MS                       10
+#define DEBOUNCE_BUTTON_TIME_MS                 40
+
+
+typedef enum {
+    BUTTON_UP,
+    BUTTON_DOWN,
+    BUTTON_FALLING,
+    BUTTON_RISING
+} buttonState_t;
+
+int accumulatedDebounceButtonTime       = 0;
+int pressed                            = 0;
+
 DigitalIn driverSeat(D2);
 DigitalIn ignition(D6);
 
@@ -12,17 +26,28 @@ DigitalOut leftHeadlight(D8);
 DigitalOut rightHeadlight(D9);
 DigitalOut lowBeams(D10);
 DigitalOut highBeams(D11);
+DigitalOut Engine(LED2);
 
 UnbufferedSerial uartUsb(USBTX, USBRX, 115200);
 
+buttonState_t IgnitionButtonState;
+
 void inputsInit();
 void outputsInit();
+void startEngine();
+void stopEngine();
+void debounceButtonInit();
+bool debounceButtonUpdate();
 
 int main() 
 {
     inputsInit();
     outputsInit();
-    while (true) {
+    while (true){
+        startEngine();
+        
+        //stopEngine();
+        delay(TIME_INCREMENT_MS);
     }
 }
 
@@ -38,4 +63,86 @@ void outputsInit()
     rightHeadlight = OFF;
     lowBeams = OFF;
     highBeams = OFF;
+}
+
+void startEngine(){
+    bool IgnitionButtonReleasedEvent = debounceButtonUpdate();
+    if(driverSeat && !Engine && IgnitionButtonReleasedEvent){
+        pressed += 1;
+        Engine = ON;
+    }
+    
+}
+
+void stopEngine(){
+    bool IgnitionButtonReleasedEvent = debounceButtonUpdate();
+    if(Engine && IgnitionButtonReleasedEvent){
+    
+        Engine = OFF;
+        pressed = 0;
+    }
+
+}
+
+bool debounceButtonUpdate()
+{
+    bool ignitionReleasedEvent = false;
+    switch( IgnitionButtonState ) {
+
+    case BUTTON_UP:
+        if( ignition == 1 ) {
+            IgnitionButtonState = BUTTON_FALLING;
+            accumulatedDebounceButtonTime = 0;
+        }
+        break;
+
+    case BUTTON_FALLING:
+        if( accumulatedDebounceButtonTime >= DEBOUNCE_BUTTON_TIME_MS ) {
+            if( ignition == 1 ) {
+                IgnitionButtonState = BUTTON_DOWN;
+            } else {
+                IgnitionButtonState = BUTTON_UP;
+            }
+        }
+        accumulatedDebounceButtonTime = accumulatedDebounceButtonTime +
+                                        TIME_INCREMENT_MS;
+        break;
+
+    case BUTTON_DOWN:
+        if( ignition == 0 ) {
+            IgnitionButtonState = BUTTON_RISING;
+            accumulatedDebounceButtonTime = 0;
+        }
+        break;
+
+    case BUTTON_RISING:
+        if( accumulatedDebounceButtonTime >= DEBOUNCE_BUTTON_TIME_MS ) {
+            if( ignition == 0 ) {
+                IgnitionButtonState = BUTTON_UP;
+                ignitionReleasedEvent = true;
+                //yyyy
+            } else {
+                IgnitionButtonState = BUTTON_DOWN;
+                //yyy
+        
+            }
+        }
+        accumulatedDebounceButtonTime = accumulatedDebounceButtonTime +
+                                        TIME_INCREMENT_MS;
+        break;
+
+    default:
+        debounceButtonInit();
+        break;
+    }
+    return ignitionReleasedEvent;
+}
+
+void debounceButtonInit()
+{
+    if( ignition == 1) {
+        IgnitionButtonState = BUTTON_UP;
+    } else {
+        IgnitionButtonState = BUTTON_DOWN;
+    }
 }
