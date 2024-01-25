aa7652a2bd7be2dfa45339a65d5ec2a220a2da40
samscan <scanlons@union.edu>
2024-01-25 17:41:50 +0000

    good

--- main.cpp
+++ main.cpp
@@ -1,3 +1,51 @@
+
+#include "mbed.h"
+#include "arm_book_lib.h"
+
+DigitalIn driverSeat(D2);
+DigitalIn ignition(D6);
+
+AnalogIn headlightMode(A0);
+AnalogIn lightLevel(A3);
+
+DigitalOut leftHeadlight(D8);
+DigitalOut rightHeadlight(D9);
+DigitalOut lowBeams(D10);
+DigitalOut highBeams(D11);
+
+UnbufferedSerial uartUsb(USBTX, USBRX, 115200);
+
+void inputsInit();
+void outputsInit();
+
+int main() 
+{
+    inputsInit();
+    outputsInit();
+    while (true) {
+        float lightLevelReading = lightLevel.read();
+        sprintf ( str, "Potentiometer: %.2f\r\n", lightLevelReading );
+        int stringLength = strlen(str);
+        uartUsb.write(str, stringLength);
+    }
+}
+
+void inputsInit()
+{
+    driverSeat.mode(PullDown);
+    ignition.mode(PullDown);
+}
+
+void outputsInit()
+{
+    leftHeadlight = OFF;
+    rightHeadlight = OFF;
+    lowBeams = OFF;
+    highBeams = OFF;
+}
+
+
+/*
 //=====[Libraries]=============================================================
 
 #include "mbed.h"
@@ -337,3 +385,4 @@ float celsiusToFahrenheit( float tempInCelsiusDegrees )
 {
     return ( tempInCelsiusDegrees * 9.0 / 5.0 + 32.0 );
 }
+*/
