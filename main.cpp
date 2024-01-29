269ab608e109ee16205b9bf240389b91be7c83b0
samscan <scanlons@union.edu>
2024-01-29 20:57:52 +0000

    some more changes

--- main.cpp
+++ main.cpp
@@ -13,24 +13,39 @@ typedef enum {
     BUTTON_RISING
 } buttonState_t;
 
+typedef enum {
+    ON_SELECTED, 
+    OFF_SELECTED,
+    AUTO_SELECTED
+} headlightSelection_t;
+
+typedef enum {
+    DAYLIGHT,
+    DUSK,
+    BETWEEN
+} lightLevelState_t;
+
 DigitalIn driverSeat(D2);
 DigitalIn ignition(D6);
 
 AnalogIn headlightMode(A0);
 AnalogIn lightLevel(A3);
 
-DigitalOut leftHeadlight(D8);
-DigitalOut rightHeadlight(D9);
-DigitalOut lowBeams(D10);
-DigitalOut highBeams(D11);
+DigitalOut leftLowBeam(D8);
+DigitalOut leftHighBeam(D9);
+DigitalOut rightLowBeam(D10);
+DigitalOut rightHighBeam(D11);
 DigitalOut engine(LED2);
 
 UnbufferedSerial uartUsb(USBTX, USBRX, 115200);
 
 buttonState_t ignitionButtonState;
+headlightSelection_t headlightState;
+lightLevelState_t lightLevelState;
 
 int accumulatedDebounceButtonTime = 0;
 int ignitionPressedDebounceTime = 0;
+int accumulatedHeadlightDelay = 0;
 
 void inputsInit();
 void outputsInit();
@@ -39,6 +54,11 @@ void checkStartEngine();
 void checkStopEngine();
 void debounceButtonInit();
 bool debounceButtonUpdate();
+void headlightUpdate();
+void headlightON();
+void headlightOFF();
+void headlightAUTO();
+void lightLevelUpdate();
 
 int main() 
 {
@@ -46,6 +66,7 @@ int main()
     outputsInit();
     while (true){
         engineUpdate();
+        headlightUpdate();
         delay(TIME_INCREMENT_MS);
     }
 }
@@ -58,10 +79,10 @@ void inputsInit()
 
 void outputsInit()
 {
-    leftHeadlight = OFF;
-    rightHeadlight = OFF;
-    lowBeams = OFF;
-    highBeams = OFF;
+    leftLowBeam = OFF;
+    rightLowBeam = OFF;
+    leftHighBeam = OFF;
+    rightHighBeam = OFF;
     engine = OFF;
 }
 
@@ -151,5 +172,69 @@ void debounceButtonInit()
         ignitionButtonState = BUTTON_UP;
     } else {
         ignitionButtonState = BUTTON_DOWN;
+    }
+}
+
+void headlightUpdate() 
+{
+    if(engine) {
+        float headlightModeValue = headlightMode.read();
+        if(headlightModeValue <= 0.333) {
+            headlightState = ON_SELECTED;
+            headlightON();
+        }
+        if(0.333 < headlightModeValue && headlightModeValue <= 0.667) {
+            headlightState = OFF_SELECTED;
+            headlightOFF();
+        }
+        if (0.667 < headlightModeValue) {
+            headlightState = AUTO_SELECTED;
+            headlightAUTO();
+        }
+    }
+    else {
+        headlightState = OFF_SELECTED;
+        headlightOFF();
+    }
+}
+
+void headlightON()
+{
+    leftLowBeam = ON;
+    rightLowBeam = ON;
+    leftHighBeam = OFF;
+    rightHighBeam = OFF; 
+}
+
+void headlightOFF()
+{
+    leftLowBeam = OFF;
+    rightLowBeam = OFF;
+    leftHighBeam = OFF;
+    rightHighBeam = OFF;
+}
+
+void headlightAUTO() 
+{
+    lightLevelUpdate();
+}
+
+void lightLevelUpdate()
+{
+    float lightLevelValue = lightLevel.read();
+    if(lightLevelValue <= 0.333) {
+        if(accumulatedHeadlightDelay >= 1000) {
+            headlightON();
+        }
+        accumulatedHeadlightDelay = accumulatedHeadlightDelay + TIME_INCREMENT_MS;
+    }
+    if(0.333 < lightLevelValue && lightLevelValue <= 0.667) {
+        accumulatedHeadlightDelay = 0;
+    }
+    if(0.667 < lightLevelValue) {
+        if(accumulatedHeadlightDelay >= 2000) {
+            headlightOFF();
+        }
+        accumulatedHeadlightDelay = accumulatedHeadlightDelay + TIME_INCREMENT_MS;
     }
 }
